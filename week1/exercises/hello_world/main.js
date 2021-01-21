f56730f985f8db174e85cebc591760b5fdda1759
afarrell392 <74276945+afarrell392@users.noreply.github.com>
2021-01-20 21:37:34 -0500

    iterating commands

--- week1/exercises/hello_world/main.js
+++ week1/exercises/hello_world/main.js
@@ -1,3 +1,4 @@
-console.log("Hello, World!");
-
-var person = prompt("Please type your name in the box below");
+console.log('Hello, World!');
+const name = window.prompt('Write your name in the box below');
+window.alert(`Hello, ${name}`);
+console.log('Hello,' + name);
