05d9384d3893a436c44352e175ea84ee7dc4248c
afarrell392 <74276945+afarrell392@users.noreply.github.com>
2021-01-28 20:21:22 -0500

    movie array

---
+++ week2/exercises/array.js
@@ -0,0 +1,12 @@
+const yourTitle = window.prompt('What is your favorite movie?');
+const movieArray = [
+  'Pirates of the Caribbean: Curse of the Black Pearl',
+  'Pirates of the Caribbean: Dead Mans Chest',
+  'Pirates of the Caribbean: At Worlds End',
+  'Pirates of the Caribbean: On Stranger Tides',
+  'Pirates of the Caribbean: Dead Men Tell No Tales'
+];
+movieArray.push(yourTitle);
+console.log(movieArray);
+console.log(movieArray.toString());
+console.log('Not all treasure is silver and gold, mate');
