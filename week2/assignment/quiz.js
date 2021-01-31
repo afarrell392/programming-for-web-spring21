d7b37e0de31c076ba91e4ce51a8f4df66c974f58
afarrell392 <74276945+afarrell392@users.noreply.github.com>
2021-01-31 18:28:58 -0500

    content and styling

--- week2/assignment/quiz.js
+++ week2/assignment/quiz.js
@@ -1,5 +1,16 @@
-var questionArray = [
-    {
-        question: 'How many worms are in the average vermi'
-    }
-]
+const questionArray = [
+  { question: 'Which two US states don\'t observe Daylight Savings Time?', answer: 'Arizona and Hawai\'i' },
+  { question: 'In which two states were the first stop signs posted in 1915?', answer: 'Michigan and Nebraska' },
+  { question: 'What was the original name of the settlement that became St. Paul, MN?', answer: 'Pig\'s Eye' },
+  { question: 'What percent of the country\'s toothpick supply is produced in Maine?', answer: '90%' },
+  { question: 'Which state was the first to enact laws protecting working women?', answer: 'Ohio, in 1852' }
+];
+console.log(questionArray);
+// randomize
+const randomNumber = Math.round(Math.random() * (questionArray.length - 1));
+console.log(questionArray[randomNumber]);
+// prompt user with question
+const questionAnswer = window.prompt(questionArray[randomNumber].question);
+console.log(questionAnswer);
+// you answered alert
+window.alert('You answered ' + questionAnswer + '.' + ' The correct answer is ' + questionArray[randomNumber].answer + '.');
