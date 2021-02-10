b7f2dd1306a5352562b576cf7ef90fed355d728e
afarrell392 <74276945+afarrell392@users.noreply.github.com>
2021-02-10 16:24:42 -0500

    completion of 4.1

--- week4/exercises/4.1_dom/dom.js
+++ week4/exercises/4.1_dom/dom.js
@@ -3,11 +3,15 @@ Append the new headline element to "myEmptyDiv".
 And an event listener to your headline to change the background color of the whole window when the headline is clicked. */
 
 // select empty div and assign to variable
-const myEmptyDiv = document.querySelector("#myEmptyDiv");
+const myEmptyDiv = document.querySelector('#myEmptyDiv');
 
 // take variable and put an h1 inside it
 // create a new element
 const heading = document.createElement('h1');
-heading.innerHTML = 'Hello, Class! It is a snowy day';
-// append child to the DOM to view
-myEmptyDiv.appendChild(heading);
+heading.innerHTML = 'Hello, Class! It is a snowy day!';
+heading.style.cursor = 'pointer'; // changes cursor to a pointer when you hover over the heading
+heading.addEventListener('click', handleHeadingClick); // changes X when the heading is clicked (in this case, the background color)
+myEmptyDiv.appendChild(heading); // append child to attach it the DOM to view
+function handleHeadingClick () {
+  document.body.style.backgroundColor = 'orange';
+}
