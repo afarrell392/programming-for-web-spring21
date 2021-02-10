8cfdef8e783038c00d7189e83ad7cf9a1a876aa8
afarrell392 <74276945+afarrell392@users.noreply.github.com>
2021-02-10 16:31:26 -0500

    forgot the llama part

--- week4/exercises/4.1_dom/dom.js
+++ week4/exercises/4.1_dom/dom.js
@@ -8,7 +8,7 @@ const myEmptyDiv = document.querySelector('#myEmptyDiv');
 // take variable and put an h1 inside it
 // create a new element
 const heading = document.createElement('h1');
-heading.innerHTML = 'Hello, Class! It is a snowy day!';
+heading.innerHTML = 'A baby llama is called a "cria" which is Spanish for baby. It\'s pronounced KREE-uh. Baby alpacas, vicuñas, and guanacos are also called crias. Mama llamas usually only have one baby at a time and llama twins are incredibly rare. Pregnancy lasts for about 350 days, nearly a full year. Crias weigh 20 to 35 pounds at birth.'; // source: https://www.thoughtco.com/fun-facts-about-llamas-3880940
 heading.style.cursor = 'pointer'; // changes cursor to a pointer when you hover over the heading
 heading.addEventListener('click', handleHeadingClick); // changes X when the heading is clicked (in this case, the background color)
 myEmptyDiv.appendChild(heading); // append child to attach it the DOM to view
