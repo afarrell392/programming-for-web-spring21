406b57b9695b46787c64cb3d52f6c179fb006aab
afarrell392 <74276945+afarrell392@users.noreply.github.com>
2021-02-18 20:25:21 -0500

    no dice with the for loop

--- week5/exercises/5.2_classes/classes.js
+++ week5/exercises/5.2_classes/classes.js
@@ -1,83 +1,41 @@
 /* eslint-disable no-undef */
 let rect1;
 let rect2;
-var i = 0;
 
 function setup () {
   createCanvas(600, 400);
-  rect1 = new Rectangle();
-  rect2 = new Rectangle();
+  rect1 = new Rectangle(250, 150, 100, 100);
+  rect2 = new Rectangle(200, 100, 50, 50);
 }
+// I couldn't figure out the for loop for drawing the rectangle. Among other attempts, I moved 'new Rectangle' into the draw function as a for loop, but I just couldn't seem to execute it.
 
 function draw () {
   background(0);
-for (i = 0; i < 2; i++){
-    rect1.move();
-    rect1.show();
-    rect2.move();
+  rect1.move();
+  rect1.show();
+  rect2.move();
   rect2.show();
 }
-}
-//   rect1.move();
-//   rect1.show();
-//   rect2.move();
-//   rect2.show();
-// }
 // classes are new blocks of code that contain everything it means to be that object: functionality, data, etc. The class is the cookie cutter.
 class Rectangle {
-  constructor () {
+  constructor (x, y, w, h) {
     // 'this' keyword references the current object
-    this.x = 200;
-    this.y = 150;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
   }
 
   move () {
     // the numbers in random are the speed at which the shape moves
-    this.x = this.x + random(-7, 7);
-    this.y = this.y + random(-7, 7);
+    this.x = this.x + random(-5, 5);
+    this.y = this.y + random(-5, 5);
   }
 
   show () {
     stroke('purple');
     strokeWeight(10);
     noFill();
-    rect(this.x, this.y, 50, 50);
+    rect(this.x, this.y, this.w, this.h);
   }
 }
-
-// let bubble1;
-// let bubble2;
-
-// function setup () {
-//   createCanvas(600, 400);
-//   bubble1 = new Bubble();
-//   bubble2 = new Bubble();
-// }
-
-// function draw () {
-//   background(0);
-//   bubble1.move();
-//   bubble1.show();
-//   bubble2.move();
-//   bubble2.show();
-// }
-// // classes are new blocks of code that contain everything it means to be that object: functionality, data, etc. The class is the cookie cutter.
-// class Bubble {
-//   constructor () {
-//     // 'this' keyword references the current object
-//     this.x = 200;
-//     this.y = 150;
-//   }
-
-//   move () {
-//     this.x = this.x + random(-5, 5);
-//     this.y = this.y + random(-5, 5);
-//   }
-
-//   show () {
-//     stroke(255);
-//     strokeWeight(4);
-//     noFill();
-//     ellipse(this.x, this.y, 24, 24);
-//   }
-// }
