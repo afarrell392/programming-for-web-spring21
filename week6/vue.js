cb79aa1dbeed69b14e72a479a50578d6c7e7597b
afarrell392 <74276945+afarrell392@users.noreply.github.com>
2021-02-28 11:47:05 -0500

    week 6 vue

---
+++ week6/vue.js
@@ -0,0 +1,35 @@
+// var llamaFacts = new Vue({
+//   el: '#llamaFacts',
+//   data: {
+//     items: [
+//       { message: 'Llama wool is light, warm, water-repellent, and free of lanolin.' },
+//       { message: 'Llamas are sure-footed and easily navigate rocky terrain at high altitudes.' },
+//       { message: "Llamas don't bite. They spit when they're agitated." },
+//       { message: 'Llamas can be used as guard animals for livestock. They are easy to train.' },
+//       { message: 'Llamas are vegetarians and have very efficient digestive systems.' },
+//       { message: 'Llamas stomachs have three compartments: the rumen, omasum, adn abomasum.' },
+//       { message: 'Llamas farmers refer to llama manure as llama beans.' },
+//       { message: 'Llamas live to be about 20 years old.' },
+//       { message: 'A baby llama is called a cria, which is Spanish for baby.' },
+//       { message: 'A group of llamas is called a herd.' }
+//     ]
+//   }
+// });
+
+var llamaFacts = new Vue({
+  el: '#llamaFacts',
+  data: {
+    items: [
+      'Llama wool is light, warm, water-repellent, and free of lanolin.' ,
+      'Llamas are sure-footed and easily navigate rocky terrain at high altitudes.',
+      "Llamas don't bite. They spit when they're agitated.",
+      'Llamas can be used as guard animals for livestock. They are easy to train.',
+      'Llamas are vegetarians and have very efficient digestive systems.',
+      'Llamas stomachs have three compartments: the rumen, omasum, adn abomasum.',
+      'Llamas farmers refer to llama manure as llama beans.',
+      'Llamas live to be about 20 years old.',
+      'A baby llama is called a cria, which is Spanish for baby.'
+    ],
+    llamaHerd: true
+  }
+});
